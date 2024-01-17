4ea05b6aa10344118f41ff7b3407a3ffcdf958cd
Bhakta <dashlucky248@gmail.com>
2024-01-17 16:50:57 +0530

    updated the apply polyfill code

--- javascript/polyfill/apply.js
+++ javascript/polyfill/apply.js
@@ -4,7 +4,7 @@ let obj ={
 }
 
 function fullname(city, state, country){
-    console.log(this.firstname + " " + this.lastname + " from " + city + " state " + state + " country " + country)
+    console.log(this.firstname + " " + this.lastname + " from " + city + " state " + state + " and country " + country) 
 }
 
 // fullname.apply(obj,['Berhampur', "Odisha"])
@@ -13,6 +13,7 @@ Function.prototype.myapply = function(obj={}, args=[]){
     let randomProp = Math.random();
     obj[randomProp] = this; 
     let result = obj[randomProp](...args); 
+    delete obj[randomProp]; 
     return result;
 }
 
